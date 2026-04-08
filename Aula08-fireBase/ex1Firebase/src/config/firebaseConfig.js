3416325da7bb6e201ae2e347735722c94195c09d
Vinicinsss <143666494+Vinicinsss@users.noreply.github.com>
2026-04-08 22:35:37 +0000

    Firebase inicio

---
+++ Aula08-fireBase/ex1Firebase/src/config/firebaseConfig.js
@@ -0,0 +1,10 @@
+import { initializeApp } from 'firebase';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD9a20FslbC2vwO7ZoH9pj5MHsoJG-uPE4",
+  authDomain: "unipam-vinicius-appteste.firebaseapp.com",
+  projectId: "unipam-vinicius-appteste",
+  storageBucket: "unipam-vinicius-appteste.firebasestorage.app",
+  messagingSenderId: "823179427605",
+  appId: "1:823179427605:web:840b8af7d46a9645e7b31c"
+};
